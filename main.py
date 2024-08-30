e275659c99f729a0002066953864cf6926cd4993
Kim Alex <kimalex2004dev@gmail.com>
2024-08-31 01:07:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ file_handler.setLevel(logging.DEBUG)  # Уровень DEBUG для файла
 
 # Создание обработчика для вывода в консоль с уровнем DEBUG
 console_handler = logging.StreamHandler()
-console_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levellevel)s - %(message)s')
+console_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 console_handler.setFormatter(console_formatter)
 console_handler.setLevel(logging.DEBUG)  # Уровень DEBUG для консоли
 
